bba4ba830fee9c9dd4a9511cd0af2b5507d10acc
Xing Zhou <xing@xingzhou.me>
2022-11-29 16:33:44 -0500

    test: use TestRestTemplate for Session API tests

--- src/test/java/me/xingzhou/fivethreeone/SessionAPITests.java
+++ src/test/java/me/xingzhou/fivethreeone/SessionAPITests.java
@@ -1,41 +1,49 @@
 package me.xingzhou.fivethreeone;
 
 import me.xingzhou.fivethreeone.entity.User;
-import org.junit.jupiter.api.Disabled;
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.net.URI;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Disabled
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SessionAPITests {
 
     @Autowired
-    private MockMvc mvc;
+    private TestRestTemplate restTemplate;
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    @DisplayName("See the session history of an user that exists.")
-    void emptySessionHistory() throws Exception {
+    @DisplayName("See the session history of an user that exists but without session history.")
+    void emptySessionHistory() {
         final User user = userRepository.save(new User());
-        mvc.perform(get("/users/" + user.getId() + "/sessions"))
-                .andExpect(status().isOk());
+        final Long id = user.getId();
+        final ResponseEntity<JSONObject> responseEntity = getUserSessions(id, JSONObject.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    private <T> ResponseEntity<T> getUserSessions(Long id, Class<T> responseType) {
+        URI url = URI.create("/users/" + id + "/sessions");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        final HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+        return restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
     }
 
     @Test
     @DisplayName("See the session history of an user that does not exist.")
-    void sessionHistoryWithNonExistentUser() throws Exception {
-        Long id = 0l;
-        mvc.perform(get("/users/" + id + "/sessions"))
-                .andExpect(status().isNotFound());
+    void sessionHistoryWithNonExistentUser() {
+        Long id = 0L;
+        final ResponseEntity<JSONObject> responseEntity = getUserSessions(id, JSONObject.class);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
     }
 }
